46319e7fc112245b4a4a212cd52694838d33dc98
shashank-1369 <47159677+shashank-1369@users.noreply.github.com>
2020-08-18 16:43:57 -0400

    Mod5

---
+++ Module 5/05.02 ASCII/Navale_05.02_ASCII.py
@@ -0,0 +1,12 @@
+# Shashank Navale
+# 08/14/2020
+# Purpose: The purpose of this program is to encode and decode a secret message
+
+def main():
+    print("The encoded message is: ")
+    print("["+str(ord("S"))+","+str(ord("h"))+","+str(ord("a"))+","+str(ord("s")) +
+          ","+str(ord("h"))+","+str(ord("a"))+","+str(ord("n"))+","+str(ord("k"))+"]")
+   
+
+
+main()
