5ae03b4e786ad4070fb5c497054ceb84ec12e2e9
shashank-1369 <shashank.navale@outlook.com>
2020-08-20 16:29:35 -0400

    Completed 05.02

--- Module 5/05.02 ASCII/Navale_05.02_ASCII.py
+++ Module 5/05.02 ASCII/Navale_05.02_ASCII.py
@@ -3,10 +3,25 @@
 # Purpose: The purpose of this program is to encode and decode a secret message
 
 def main():
-    print("The encoded message is: ")
-    print("["+str(ord("S"))+","+str(ord("h"))+","+str(ord("a"))+","+str(ord("s")) +
-          ","+str(ord("h"))+","+str(ord("a"))+","+str(ord("n"))+","+str(ord("k"))+"]")
-   
+	secretMessage = "I solemnly swear I am upto no good."
+	encodedMessage = []
+
+	print("The encoded message is :")
+
+	for m in secretMessage:
+		encodedMessage.append(ord(m))
+
+	for c in encodedMessage:
+		print(c)
+
+	print("The decoded message is :")
+
+	decodedMessage = ""
+
+	for d in encodedMessage:
+		decodedMessage = decodedMessage + chr(d)
+
+	print(decodedMessage)
 
 
 main()
