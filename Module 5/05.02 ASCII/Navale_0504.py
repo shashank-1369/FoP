23d133b3bb08c80445bb756fa2b7a727009013df
shashank-1369 <shashank.navale@outlook.com>
2020-08-20 00:14:12 -0400

    Create Navale_0504.py

---
+++ Module 5/05.02 ASCII/Navale_0504.py
@@ -0,0 +1,24 @@
+# Shashank Navale
+# 08/20/2020
+# Purpose: To implement OOP programming, that is classes and methods to print a description of my superhero
+
+class Superhero:
+
+    # Superhero class represents the facts related to a superhero.
+
+
+def __init__(self, name="", strengthPts=0):
+
+    # Create a new Superhero with a name and other attributes.
+
+
+    self.name = name
+    self.strengthPts = strengthPts
+
+
+def addStrengthPts(self, points):
+
+    # Adds points to the superhero's strength.
+
+
+self.strengthPts = self.strengthPts + points
