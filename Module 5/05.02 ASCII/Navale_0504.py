c726ef7f5b208d7ea4f0bd78a630ad193dbfb281
shashank-1369 <shashank.navale@outlook.com>
2020-08-20 23:02:06 -0400

    Updated the 0504 file

--- Module 5/05.02 ASCII/Navale_0504.py
+++ Module 5/05.02 ASCII/Navale_0504.py
@@ -21,4 +21,44 @@ class Superhero:
 
         self.strenghtPts = self.strenghtPts + points
 
+    def userInput(self, sigAbility, secondaryAbility):
+
+    	# allows the user to input and control how the superhero reacts to the oppenent he/she is fighting
+
+    	if(userIn == "Blaze"):
+    		print(newSuperhero.sigAbility)
+
+    	elif(userIn == "Hot Hands"):
+    		print(newSuperhero.secondaryAbility)
+
+def main():
+
+		# create new superhero called Phoenix and utilize its attributes
+
+    	newSuperhero = Superhero("Phoenix", 0, "Blaze", "Hot Hands")
+    	print("Let me tell you about my superhero :")
+    	print("His name is "+newSuperhero.name+" and his signature ability is "+newSuperhero.sigAbility+".")
+    	print("He also has a secondary ability called "+newSuperhero.secondaryAbility+".")
+
+    	# create the villain called Omen and utilize his attributes
+
+    	newVillain = Superhero("Omen", 0, "Teleport", "Paranoya")
+    	print("Oh No! Looks like "+newSuperhero.name+" is facing his rival villain "+newVillain.name+"!")
+    	print(newVillain.name+"'s signature ability is that he can "+newVillain.sigAbility+"!")
+    	print(newVillain.name+" also has a secondary ability called "+newVillain.secondaryAbility+".")
+    	print("Looks like Omen has sent a "+newVillain.secondaryAbility+ " towards "+newSuperhero.name+" to disrupt his senses.")
+    	print("It is crucial that "+newSuperhero.name+" makes the right decision!")
+    	print("Can you help him decide if he should use his signature ability, "+newSuperhero.sigAbility+" or his secandary ability, "+newSuperhero.secondaryAbility+"?")
+    	userIn = input("Please input 'Blaze' to use his signature ability or 'Hot Hands' to use his secondary ability.")
+
+    	# call the userInput function
+    	newSuperhero.userInput()
+
+
+
+main()
+
+
+
+
 
