e79e48f3e3f3a5a992e8fb803197014a89a47cd0
snavale29 <shashank.navale@outlook.com>
2020-11-16 07:54:36 -0500

    Just a quick update with the intro

--- Module 6/Nava;e_6.03.py
+++ Module 6/Nava;e_6.03.py
@@ -1,2 +1,16 @@
 #Shashank Navale
 #Purpose: To create a program that plays Rock Paper Scissors with the user and the computer has 100% win rate 
+
+
+def main():
+    print("Welcome to the RPS Bot!")
+    print(" You can play Rock Paper Scissors with for as long as you want and I will not get bored unlike a human :)")
+
+
+
+
+
+
+
+
+main()
