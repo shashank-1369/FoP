b1090d2e0a34aa4a7f8184912bd6406ab506de35
snavale29 <shashank.navale@outlook.com>
2020-11-23 10:07:47 -0500

    Final Commit with time delays and time stamps

--- Module 6/Nava;e_6.03.py
+++ Module 6/Nava;e_6.03.py
@@ -3,28 +3,34 @@
 
 
 def main():
+    import time
+    import datetime
     print("Welcome to the RPS Bot!")
     print(" You can play Rock Paper Scissors with for as long as you want and I will not get bored unlike a human :)")
 
     playerInput = input("Please enter R for Rock, P for Paper and S for Scissors")
     
-    x = 100
+    localTime = time.time()
 
 
-        if (playerInput == "R"):
+    if (playerInput == "R"):
             
             print("Your input: Rock")
+            time.sleep(1)
             print("Computer's Input: Paper")
-        if (playerInput == "P"):
+    if (playerInput == "P"):
             
             print("Your input: Paper")
+            time.sleep(1)
             print("Computer's Input: Scissors")
-        if (playerInput == "S"):
+    if (playerInput == "S"):
             
             print("Your input: Scissors")
+            time.sleep(1)
             print("Computer's Input: Rock")
+    time.sleep(2)
     print("You loose! Try again!")
-
-    x = x+1
+    convertedTime = datetime.datetime.fromtimestamp(localTime).strftime('%c')
+    print("Timestamp: ", convertedTime)
 
 main()
