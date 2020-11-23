ad9e9892fcd713f0bc15fd57d8e15c1c37b86668
snavale29 <shashank.navale@outlook.com>
2020-11-23 09:40:59 -0500

    draft code

--- Module 6/Nava;e_6.03.py
+++ Module 6/Nava;e_6.03.py
@@ -6,11 +6,23 @@ def main():
     print("Welcome to the RPS Bot!")
     print(" You can play Rock Paper Scissors with for as long as you want and I will not get bored unlike a human :)")
 
+    playerInput = input("Please enter R for Rock, P for Paper and S for Scissors")
+    
+    x = 0
 
-
-
-
-
-
+    while (x == 0):
+        if (playerInput == "R"):
+            
+            print("Your input: Rock")
+            print("Computer's Input: Paper")
+        if (playerInput == "P"):
+            
+            print("Your input: Paper")
+            print("Computer's Input: Scissors")
+        if (playerInput == "S"):
+            
+            print("Your input: Scissors")
+            print("Computer's Input: Rock")
+    print("You loose! Try again!")
 
 main()
